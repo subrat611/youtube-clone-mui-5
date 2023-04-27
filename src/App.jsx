6af63fd72e6effa6dbd098b492583423cac7e994
subrat <subratj611@gmail.com>
2023-04-27 23:52:18 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <Box sx={{ backgroundColor: "#000", color: "#fff" }}>
       <p>Youtube Clone with MUI-5</p>
-    </>
-  ); 
+      <Routes>
+        <Route path="/" element={<h1>NavBar</h1>} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
